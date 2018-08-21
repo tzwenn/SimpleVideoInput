ebf85ad5bdde5fff91907da9d014c958f3fa2f82
erba <eric.bachard@free.fr>
2018-08-21 16:25:39 +0200

    Improved the example (CV_WINDOW_NORMAL + resize to 1280x720 b default)

--- examples/basic_example.cpp
+++ examples/basic_example.cpp
@@ -12,10 +12,13 @@ int main(int argc, char *argv[])
 	svi::SimpleVideoInput v(argv[1]);
 	cv::Mat image;
 	int count = 0;
+        cv::namedWindow("SimpleVideoInput", CV_WINDOW_NORMAL|CV_WINDOW_OPENGL);
+        cv::resizeWindow("SimpleVideoInput", 1280, 720);
+
 	while (v.read(image)) {
 		std::cout << "Read Frame #" << count++ << " (" << v.millisecondsPerFrame() << "ms)" << std::endl;
 
-		cv::imshow("Current frame", image);
+		cv::imshow("SimpleVideoInput", image);
 		if (cv::waitKey(v.millisecondsPerFrame()) == 'q')
 			break;
 	}
